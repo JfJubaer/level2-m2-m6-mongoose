8df9c919459b97f8f8ce2c22d940de6298732c65
JfJubaer <skk5727@gmail.com>
2024-03-05 13:21:01 +0600

    first commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import app from "./app";
+import mongoose from "mongoose";
+
+const port = 3000;
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  try {
+    await mongoose.connect("mongodb://localhost:27017");
+    console.log(`🛢 Database connection successful`);
+
+    app.listen(port, () => {
+      console.log(`Server is  listening on port ${port}`);
+    });
+  } catch (err) {
+    console.log(`Failed to connect database`, err);
+  }
+}
